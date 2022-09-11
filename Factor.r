e6fa2219d7ad71fa9008e50ab8a9a22559e28854
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-11 22:16:01 +0530

    Updates

--- Factor.r
+++ Factor.r
@@ -5,13 +5,16 @@
   print(is.factor(marks))
   print(is.factor(fac)) #already factored data
   print(is.factor(data))
-  
-  
   fac_marks = factor(marks)
   fac_data = factor(data)
-  
   print(fac_marks)
   print(fac_data)
-  
   print(is.factor(fac_marks))
   print(is.factor(fac_data))
+
+  #data frame
+    framedata = data.frame(names,age,gender)
+    print(is.factor(framedata))
+    print(is.factor(framedata$gender))
+    print(framedata$gender)
+    print(framedata)
