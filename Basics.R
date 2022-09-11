8b67dd934b130a0ef3240931ea4ac9847fa3770c
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-11 14:04:30 +0530

    Ups

--- Basics.R
+++ Basics.R
@@ -4,4 +4,21 @@ b = c(14,12)
 l1 = list(a,b)
 print(l1)
 
-plot(a,)
+plot(a)
+
+install.packages("pacman")
+library(pacman)
+
+#More Packages -> CRAN, Crantastic, Github
+#dplyr -> manipulating database
+#tidyr -> cleaning up info
+#stringr -> working with strings or text info
+#lubridate -> manipulating date info
+#ttr -> for working with sites data 
+#ggvis -> interactive viz
+#ggplot2 -> for creating graphics or data viz
+#shiny -> interactive apps that can be installed on websited 
+#rio -> r ip or op , for importing and exporting data 
+#rmarkdown -> Interactive notebooks rich docs
+
+#ONE PACKAGE TO LOAD ALL PACKAGES -> pacman
