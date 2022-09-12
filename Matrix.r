80fe8b7556c78e6a234849b26d15a18fc9ea6229
Harsh Shrivastava <62792682+hmarshmello@users.noreply.github.com>
2022-09-13 00:53:35 +0530

    Changes

--- Matrix.r
+++ Matrix.r
@@ -6,6 +6,18 @@
   N = matrix(c(3:14), nrow = 4, byrow = FALSE) #Arranged sequentially by col
     print(N)
     
+  apply(N,1,sum) # N -> Data, 1/0 -> rows or cols, sum -> function to perform
+    
+  #APPLY example
+    ratings <- c(4.2, 4.4, 3.4, 3.9, 5, 4.1, 3.2, 3.9, 4.6, 4.8, 5, 4, 4.5, 3.9, 4.7, 3.6)
+    mat <- matrix(ratings,byrow=TRUE,nrow=4,dimnames = list(c("Quarter1","Quarter2","Quarter3","Quarter4"),c("Hari","Shri","John","Albert")))#user defined
+    check<-function(x){
+      return(x[x>4.2])
+    }
+    result <- apply(mat,2,check)
+    print(mat)
+    print(result)
+  
    rnames = c("R1","R2","R3","R4")
    cnames = c("C1","C2","C3")
   P = matrix(c(3:14), nrow = 4, byrow = TRUE, dimnames = list(rnames, cnames))    
